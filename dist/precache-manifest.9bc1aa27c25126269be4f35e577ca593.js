440ada64dc879506c0fbeb72361671486f74dd5e
Fabrício M. Damasceno <fabricio.damasceno@outlook.com.br>
2021-11-08 00:43:52 -0300

    Enviando projeto para Netlify

---
+++ dist/precache-manifest.9bc1aa27c25126269be4f35e577ca593.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d69d40f834fe3c9a864",
+    "url": "/css/chunk-vendors.90547b80.css"
+  },
+  {
+    "revision": "63a7d78d42c33b94fc7b957524795cac",
+    "url": "/img/logo.63a7d78d.svg"
+  },
+  {
+    "revision": "d80b904b16f69474ff9427d7da7bcec0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6f0fd97a1d3486f2b6d3",
+    "url": "/js/about.bd9d3d86.js"
+  },
+  {
+    "revision": "54bc860cf54b67cba987",
+    "url": "/js/app.61b05f74.js"
+  },
+  {
+    "revision": "7d69d40f834fe3c9a864",
+    "url": "/js/chunk-vendors.df8f2b9a.js"
+  },
+  {
+    "revision": "705b37a3509c2a81baede3e9613a50c9",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
